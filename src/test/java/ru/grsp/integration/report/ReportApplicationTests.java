b920b08e8f4d7f8deb05f62349bdf8b4fdbcc5a8
Iskander Ignatev <iskander.ignatev@gmail.com>
2017-12-07 00:49:30 +0300

    add spock

--- src/test/java/ru/grsp/integration/report/ReportApplicationTests.java
+++ src/test/java/ru/grsp/integration/report/ReportApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ReportApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
