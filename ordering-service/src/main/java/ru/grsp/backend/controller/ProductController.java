5676cf4f7fc17473c7a3ead1c5b6f57d37c886a3
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-05-20 02:02:58 +0300

    access db using hibernate+spring-data

---
+++ ordering-service/src/main/java/ru/grsp/backend/controller/ProductController.java
@@ -0,0 +1,22 @@
+package ru.grsp.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.grsp.backend.model.Product;
+import ru.grsp.backend.model.repository.ProductRepository;
+
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "orderProducts")
+    public @ResponseBody
+    Optional<Product> getAllOrderProducts() {
+        return productRepository.findById(75);
+    }
+}
